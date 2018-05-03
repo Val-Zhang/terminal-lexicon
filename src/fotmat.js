737353ea6352d07a07815b8a3e378b023348a9f7
zhangwang <zhangwang2014@iCloud.com>
2018-05-03 13:43:19 +0800

    添加单词的主体释义

--- src/fotmat.js
+++ src/fotmat.js
@@ -3,9 +3,16 @@ const chalk = require("chalk");
 function formatSoundmark(markDict = {}) {
   let formatedSoundmark = "";
   for (let mark in markDict) {
-    formatedSoundmark += `${chalk.blue(mark)}  [${chalk.bgBlue(markDict[mark])}]     `;
+    formatedSoundmark += `${chalk.blue(mark)}  [${chalk.bgBlue(
+      markDict[mark]
+    )}]     `;
   }
   console.log(formatedSoundmark);
 }
 
+function formatDefinition(definition = "") {
+  console.log(definition);
+}
+
 module.exports.formatSoundmark = formatSoundmark;
+module.exports.formatDefinition = formatDefinition;
--- src/search.js
+++ src/search.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const { formatSoundmark } = require("./fotmat");
+const { formatSoundmark, formatDefinition } = require("./fotmat");
 
 const SOURCE_SHANBEY_API = require("../config").SOURCE_SHANBEY_API;
 
@@ -8,7 +8,9 @@ const searchWord = word => {
   axios
     .get(`${SOURCE_SHANBEY_API}${word}`)
     .then(res => {
-      formatSoundmark(res.data.data.pronunciations);
+      const data = res.data.data;
+      formatSoundmark(data.pronunciations);
+      formatDefinition(data.definition);
     })
     .catch(err => {
       console.log(err);
