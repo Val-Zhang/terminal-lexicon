d53279c4aefd740700683d5301cbcc7b6e1ff352
zhangwang <zhangwang2014@iCloud.com>
2018-05-06 08:58:08 +0800

    调整依赖

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Dict in terminal
 
 在终端中查单词
+
+-[x] 在线版
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 #! /usr/bin/env node
 
 const program = require("commander");
-const inquirer = require("inquirer");
 
 const searchWord = require("./search").searchWord;
 const VERSION = require("../config").VERSION;
